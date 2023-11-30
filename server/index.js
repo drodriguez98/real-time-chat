120e553f815e2e0d93d60eff4d9747e566c43e35
Diego Rodriguez <diego25598@gmail.com>
2023-11-30 16:15:07 +0100

    explain code

--- server/index.js
+++ server/index.js
@@ -9,27 +9,23 @@ import dotenv from 'dotenv'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
-// Configura variables de entorno
+// Cargar y configurar las variables de entorno definidas en el archivo .env
 
 dotenv.config()
 
-// Define el puerto del servidor
+// Definir el puerto del servidor
 
 const port = process.env.PORT ?? 3000
 
-// Crea la aplicación Express
+// Crear el servidor HTTP con Express. 
+// Permite manejar las rutas, las solicitudes HTTP y configurar la lógica de la aplicación (gestión de solicitudes, respuestas, middleware...).
+// Crear una instancia de Socket.IO en el servidor con reconexión y restauración de estado en caso de desconexiones intermitentes.
 
 const app = express()
-
-// Crea el servidor HTTP con Express
-
 const server = createServer(app)
-
-// Crea una instancia de Socket.IO en el servidor
-
 const io = new Server(server, { connectionStateRecovery: {} })
 
-// Conecta a la base de datos MySQL
+// Conectar a la base de datos MySQL y crear la tabla messages en la base de datos si no existe
 
 const db = await mysql.createConnection({
 
@@ -38,36 +34,35 @@ const db = await mysql.createConnection({
   password: '',
   database: 'chatdb',
   port: 3306
-  
-})
 
-// Crea la tabla messages en la base de datos si no existe
+})
 
 await db.execute('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTO_INCREMENT, content TEXT, user TEXT)')
 
-// Evento cuando se conecta un cliente al servidor mediante Socket.IO
+// Evento cuando se conecta o desconencta un cliente al servidor mediante Socket.IO
 
 io.on('connection', async (socket) => {
 
-  console.log('a user has connected!') // Imprime un mensaje cuando un usuario se conecta
+  console.log('new user connected!') 
+  socket.on('disconnect', () => { console.log('user disconnected') }) 
 
-  // Evento cuando un cliente se desconecta
-
-  socket.on('disconnect', () => { console.log('an user has disconnected') }) // Imprime un mensaje cuando un usuario se desconecta
-
-  // Evento para recibir un mensaje del cliente y guardarlo en la base de datos
+  // Evento cuando el servidor recibe un mensaje desde el cliente.
 
   socket.on('chat message', async (msg) => {
 
+    // Obtener el nombre de usuario del handshake inicial del cliente emisor.
+
+    const username = socket.handshake.auth.username ?? 'anonymous' // console.log({ username }) 
+
+    // Guardar el mensaje en la base de datos.
+    
     let result
-    const username = socket.handshake.auth.username ?? 'anonymous' // Obtiene el nombre de usuario del cliente
-    console.log({ username }) // Imprime el nombre de usuario en la consola
 
     try {
 
       result = await db.execute({
 
-        sql: 'INSERT INTO messages (content, user) VALUES (?, ?)', // Inserta el mensaje en la base de datos
+        sql: 'INSERT INTO messages (content, user) VALUES (?, ?)',
         values: [msg, username]
 
       })
@@ -79,13 +74,13 @@ io.on('connection', async (socket) => {
       
     }
 
-    // Emite el mensaje a todos los clientes conectados junto con su ID, nombre de usuario
+    // Reenviar el mensaje a todos los clientes conectados junto con su ID y nombre de usuario
 
     io.emit('chat message', msg, result[0].insertId.toString(), username)
 
   })
 
-  // Recupera mensajes del historial para un nuevo cliente que se conecta
+  // Recuperar y mostrar mensajes del historial para un nuevo cliente que se conecta 
 
   if (!socket.recovered) {
 
@@ -96,24 +91,25 @@ io.on('connection', async (socket) => {
       // Emite mensajes recuperados al cliente recién conectado
 
       results.forEach(row => { socket.emit('chat message', row.content, row.id.toString(), row.user) })
+
     } catch (e) { console.error(e) }
 
   }
 
 })
 
-// Configura el middleware de registro de solicitudes HTTP
+// Configurar el middleware de registro de solicitudes HTTP (morgan)
 
 app.use(logger('dev'))
 
-// Sirve archivos estáticos desde la carpeta 'client'
+// Servir archivos estáticos (html, css...) desde la carpeta 'client'
 
 app.use(express.static(process.cwd() + '/client'))
 
-// Ruta principal que sirve el archivo index.html
+// Añadir ruta principal que sirve el archivo index.html
 
-app.get('/', (req, res) => { res.sendFile(process.cwd() + '/client/index.html') })
+app.get('/', (req, res) => { res.sendFile('index.html') })
 
-// Inicia el servidor en el puerto especificado
+// Iniciar el servidor en el puerto especificado
 
-server.listen(port, () => { console.log(`Server running on port ${port}`) }) // Imprime un mensaje cuando el servidor se inicia
+server.listen(port, () => { console.log(`Server running on port ${port}`) })
