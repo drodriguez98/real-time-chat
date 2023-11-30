00aa46f075dd4907007ce7d22c0c985f8c7b646c
Diego Rodriguez <diego25598@gmail.com>
2023-11-30 14:49:22 +0100

    update create database script

--- chat.sql
+++ chat.sql
@@ -1,11 +1,19 @@
+// SCRIPT PARA CREAR LA BASE DE DATOS
+
+// Elimina la base de datos si existe y crea una nueva llamada chatdb
 DROP DATABASE IF EXISTS chatdb;
-
 CREATE DATABASE chatdb;
-
 USE chatdb;
 
-CREATE TABLE IF NOT EXISTS  messages ( id INTEGER PRIMARY KEY AUTO_INCREMENT, content TEXT, user VARCHAR(255) )
+// Crea una tabla llamada messages con columnas id, content y user
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  content TEXT,
+  user VARCHAR(255)
+)
 
-CREATE USER 'chat'@'localhost' IDENTIFIED BY ''; -- Crea el usuario sin contraseña
+// Crea un usuario 'chat' sin contraseña
+CREATE USER 'chat'@'localhost' IDENTIFIED BY '';
 
+// Otorga todos los privilegios al usuario 'chat' en cualquier base de datos
 GRANT ALL PRIVILEGES ON *.* TO 'chat'@'localhost' WITH GRANT OPTION;
