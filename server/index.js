5c1139387aab6b56cd6faf301c43baad0b9a08ea
Diego Rodriguez <diego25598@gmail.com>
2023-11-30 01:27:17 +0100

    communicate client with server

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ const io = new Server(server)
 
 io.on('connection', (socket) => {
   console.log('new user connected!')
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on('disconnect', () => { console.log('user disconnected') })
+  socket.on('chat message', (msg) => {
+    console.log('new message: ' + msg)
   })
 })
 
